fe345aae53e7b3ad9296a58fac0405f42b29006c
Nimo Beeren <nimobeeren@gmail.com>
2025-07-22 15:06:25 +0200

    add a tool

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
+import {
+  Agent,
+  run,
+  setDefaultOpenAIClient,
+  setOpenAIAPI,
+  setTracingDisabled,
+  tool,
+} from "@openai/agents";
 import "dotenv/config";
-import { Agent, run, setDefaultOpenAIClient, setOpenAIAPI, setTracingDisabled } from "@openai/agents";
 import { AzureOpenAI } from "openai";
+import { z } from "zod";
 
 const openai = new AzureOpenAI();
 setDefaultOpenAIClient(openai);
@@ -9,8 +17,16 @@ setTracingDisabled(true);
 
 const agent = new Agent({
   name: "Single Agent",
-  instructions: "You are a helpful assistant"
+  instructions: "You are a helpful assistant",
+  tools: [
+    tool({
+      name: "addNumbers",
+      description: "add two numbers together",
+      parameters: z.object({ a: z.number(), b: z.number() }),
+      execute: async ({ a, b }) => a + b,
+    }),
+  ],
 });
 
-const result = await run(agent, "Write a haiku about recursion in programming")
+const result = await run(agent, "What's 2+2?");
 console.log(result.finalOutput);
